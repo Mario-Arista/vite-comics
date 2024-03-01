9d0d2970254c6d0a610100c7a584b8f08f8265c3
mario.arista <mario.arista.dev@gmail.com>
2024-03-01 15:18:10 +0100

    comics almost done

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,91 @@
 <script>
+import ComicItem from './ComicItem.vue';
 
 export default {
     name: 'AppMain',
+
+    components: {
+        ComicItem,
+    },
+
+    data() {
+        return {
+            comics: [
+                {
+                    thumb: "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    price: "$19.99",
+                    series: "Action Comics",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    price: "$3.99",
+                    series: "American Vampire 1976",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    price: "$16.99",
+                    series: "Aquaman",
+                    type: "graphic novel",
+                },
+                {
+                    thumb: "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    price: "$2.99",
+                    series: "Batgirl",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    price: "$3.99",
+                    series: "Batman",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                    price: "$2.99",
+                    series: "Batman Beyond",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                    price: "$3.99",
+                    series: "Batman/Superman",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                    price: "$4.99",
+                    series: "Batman/Superman Annual",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                    price: "$5.99",
+                    series: "Batman: The Joker War Zone",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                    price: "$6.99",
+                    series: "Batman: Three Jokers",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                    price: "$4.99",
+                    series: "Batman: White Knight Presents: Harley Quinn",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                    price: "$16.99",
+                    series: "Catwoman",
+                    type: "graphic novel",
+                },
+            ],     
+        }
+}
 
 }
 
@@ -17,38 +101,16 @@ export default {
             <div class="container">
                 <h2>current series</h2>
 
-                <div class="comic">
-                    <img src="/img/dc-logo.png" alt="">
-                    <h4>titolo</h4>
-                </div>
-                <div class="comic">
-                    <img src="/img/dc-logo.png" alt="">
-                    <h4>titolo</h4>
-                </div>
-                <div class="comic">
-                    <img src="/img/dc-logo.png" alt="">
-                    <h4>titolo</h4>
-                </div>
-                <div class="comic">
-                    <img src="/img/dc-logo.png" alt="">
-                    <h4>titolo</h4>
-                </div>
-                <div class="comic">
-                    <img src="/img/dc-logo.png" alt="">
-                    <h4>titolo</h4>
-                </div>
-                <div class="comic">
-                    <img src="/img/dc-logo.png" alt="">
-                    <h4>titolo</h4>
-                </div>
-                <div class="comic">
-                    <img src="/img/dc-logo.png" alt="">
-                    <h4>titolo</h4>
-                </div>
-                <div class="comic">
-                    <img src="/img/dc-logo.png" alt="">
-                    <h4>titolo</h4>
-                </div>
+                <ComicItem 
+                    v-for="currentComic in comics"
+                    :ComicImage="currentComic.thumb"
+                    :ComicPrice="currentComic.price"
+                    :ComicType="currentComic.type"
+                    :ComicSeries="currentComic.series"
+                >
+
+                </ComicItem>
+
                 
             </div>
         </div>
@@ -82,26 +144,14 @@ main {
             flex-wrap: wrap;
             gap: 40px 20px;
 
-            // test
-            height: 400px;
-
-            .comic {
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-
-                width: calc(100% / 6 - 20px / 6 * 5);
-
-                // test
-                border: 1px solid red;
-            }
+            padding: 40px 0px;
 
             h2 {
                 position: absolute;
                 left: 0px;
-                top: -20px;
+                top: -22px;
 
-                padding: 12px 18px;
+                padding: 12px 16px;
 
                 text-transform: uppercase;
                 font-size: 1.3em;
---
+++ src/components/ComicItem.vue
@@ -0,0 +1,87 @@
+<script>
+
+export default {
+    name: 'ComicItem',
+
+    props: {
+
+        ComicImage: String,
+        ComicPrice: String,
+        ComicType: String,
+        ComicSeries: String,
+
+    },
+
+
+}
+
+</script>
+
+<template>
+  
+    <div class="comic">
+
+        <img :src="ComicImage" :alt="ComicSeries">
+
+        <h4>{{ ComicSeries }}</h4>
+
+        <span>{{ ComicType }}</span>
+
+        <div class="price">{{ ComicPrice }}</div>
+    </div>
+
+  
+</template>
+
+<style lang="scss">
+
+@use '../styles/variables.scss' as *;
+
+.comic {
+    position: relative;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: calc(100% / 6 - 20px / 6 * 5);
+
+    h4 {
+        color: white;
+        text-transform: uppercase;
+        font-size: 0.9em;
+
+        padding-top: 10px;
+    }
+    
+    img {
+        display: block;
+        width: 180px;
+        height: 180px;
+        object-fit: cover;
+        object-position: top;
+    }
+
+    .price {
+        position: absolute;
+        left: -5px;
+        top: -5px;
+
+        padding: 2px 4px;
+
+        font-size: 0.8em;
+        font-weight: bold;
+
+        color: white;
+        background-color: $primary_color;
+    }
+
+    span {
+
+        display: none;
+    }
+
+
+}
+
+</style>
