fd9eecf7ebfcaff501e79e11faae545f89121c2d
mario.arista <mario.arista.dev@gmail.com>
2024-03-01 13:10:44 +0100

    starting main  with comics

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,8 +10,49 @@ export default {
 <template>
   
     <main>
+        <div id="bg-main"></div>
 
-        <div class="container">Content goes here!</div>
+        <div id="comics">
+
+            <div class="container">
+                <h2>current series</h2>
+
+                <div class="comic">
+                    <img src="/img/dc-logo.png" alt="">
+                    <h4>titolo</h4>
+                </div>
+                <div class="comic">
+                    <img src="/img/dc-logo.png" alt="">
+                    <h4>titolo</h4>
+                </div>
+                <div class="comic">
+                    <img src="/img/dc-logo.png" alt="">
+                    <h4>titolo</h4>
+                </div>
+                <div class="comic">
+                    <img src="/img/dc-logo.png" alt="">
+                    <h4>titolo</h4>
+                </div>
+                <div class="comic">
+                    <img src="/img/dc-logo.png" alt="">
+                    <h4>titolo</h4>
+                </div>
+                <div class="comic">
+                    <img src="/img/dc-logo.png" alt="">
+                    <h4>titolo</h4>
+                </div>
+                <div class="comic">
+                    <img src="/img/dc-logo.png" alt="">
+                    <h4>titolo</h4>
+                </div>
+                <div class="comic">
+                    <img src="/img/dc-logo.png" alt="">
+                    <h4>titolo</h4>
+                </div>
+                
+            </div>
+        </div>
+
 
     </main>
 
@@ -24,19 +65,56 @@ export default {
 
 main {
 
-    width: 100%;
-
     background-color: $sec_color;
-   
-    .container {
 
-        padding-top: 50px;
-        padding-bottom: 50px;
-
-        font-size: 1.2em;
-        color: white;
-        
+    #bg-main {
+        background-image: url('/img/jumbotron.jpg');
+        background-size: cover;
+        background-position: top;
+        width: 100%;
+        height: 400px;
     }
+
+    #comics {
+
+        .container {
+            position: relative;
+            flex-wrap: wrap;
+            gap: 40px 20px;
+
+            // test
+            height: 400px;
+
+            .comic {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+
+                width: calc(100% / 6 - 20px / 6 * 5);
+
+                // test
+                border: 1px solid red;
+            }
+
+            h2 {
+                position: absolute;
+                left: 0px;
+                top: -20px;
+
+                padding: 12px 18px;
+
+                text-transform: uppercase;
+                font-size: 1.3em;
+
+                background-color: $primary_color;
+                color: white;
+
+            }
+
+        }
+
+    }
+
 }
 
 </style>
